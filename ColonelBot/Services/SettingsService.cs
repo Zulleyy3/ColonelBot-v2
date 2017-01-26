b7dba8852b7923795fd2f93f904da0b4fe00388a
Bryan Harmon <megamasterx@outlook.com>
2017-01-26 11:32:04 -0500

    Module groundwork

---
+++ ColonelBot/Services/SettingsService.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using Discord;
+using Discord.Modules;
+using System.IO;
+using System.Collections.Concurrent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColonelBot;
+
+//Based off of RogueExceptions' DiscordBot example. 
+
+namespace ColonelBot.Services
+{
+    class SettingsService
+    {
+        public class SettingsManager<GlobalSettingsT, SettingsT> : SettingsManager<SettingsT>
+            where GlobalSettingsT : class, new()
+            where SettingsT : class, new()
+        {
+            public GlobalSettingsT Global { get; private set; }
+            public SettingsManager(string name)
+               { }
+
+            public  void LoadConfigs()
+            {
+                string path = FileTools.BotDirectory + "/global.json";
+                if (File.Exists(path))
+                    Global = JsonConvert.DeserializeObject<GlobalSettingsT>(File.ReadAllText(path));
+                else
+                    Global = new GlobalSettingsT();
+            }
+
+            public async Task SaveGlobal(GlobalSettingsT settings)
+            {
+                while (true)
+                    try
+                    {
+                        using (var fs = new FileStream(ColonelBot.FileTools.BotDirectory + "/global.json", FileMode.Create, FileAccess.Write, FileShare.None))
+                        using (var writer = new StreamWriter(fs))
+                            await writer.WriteAsync(JsonConvert.SerializeObject(Global));
+                        break;
+                    }
+                    catch (IOException)
+                    {
+
+                        await Task.Delay(1000);
+                    }
+            }
+
+            public class SettingsManager<SettingsT> 
+                where SettingsT : class, new()
+            {
+                public string Directory => _dir;
+                protected readonly string _dir;
+
+                public IEnumerable<KeyValuePair<ulong, SettingsT>> AllServers => _servers;
+                private ConcurrentDictionary<ulong, SettingsT> _servers;
+
+                public SettingsManager(string name)
+                {
+                    _dir = ColonelBot.FileTools.BotDirectory + "/Config/{name}";
+                    System.IO.Directory.CreateDirectory(_dir);
+                    LoadConfigs();
+                }
+
+                public bool RemoveServer(ulong id)
+                {
+                    SettingsT settings;
+                    if (_servers.TryRemove(id, out settings))
+                    {
+                        var path = $"{_dir}/{id}.json";
+                        if (File.Exists(path))
+                            File.Delete(path);
+                        return true;
+                    }
+                    return false;
+                }
+
+                public virtual void LoadConfigs()
+                {
+                    var servers = System.IO.Directory.GetFiles(_dir)
+                        .Select(x =>
+                        {
+                            ulong id;
+                            if (ulong.TryParse(Path.GetFileNameWithoutExtension(x), out id))
+                                return id;
+                            else
+                                return (ulong?)null;
+                        })
+                        .Where(x => x.HasValue)
+                        .ToDictionary(x => x.Value, x =>
+                        {
+                            string path = $"{_dir}/{x}.json";
+                            if (File.Exists(path))
+                                return JsonConvert.DeserializeObject<SettingsT>(File.ReadAllText(path));
+                            else
+                                return new SettingsT();
+                        });
+                    _servers = new ConcurrentDictionary<ulong, SettingsT>(servers);
+                }
+
+                public SettingsT Load(Server server) 
+                    => Load(server.Id);
+
+                public SettingsT Load(ulong serverID)
+                {
+                    SettingsT result;
+                    if (_servers.TryGetValue(serverID, out result))
+                        return result;
+                    else
+                        return new SettingsT();
+                }
+
+                public Task Save(Server server, SettingsT settings) => Save(server.Id, settings);
+                public Task Save(KeyValuePair<ulong, SettingsT> pair) => Save(pair.Key, pair.Value);
+
+                public async Task Save(ulong serverId, SettingsT settings)
+                {
+                    _servers[serverId] = settings;
+                    while (true)
+                    {
+                        try
+                        {
+                            using (var fs = new FileStream($"{_dir}/{serverId}.json", FileMode.Create, FileAccess.Write, FileShare.None))
+                            using (var writer = new StreamWriter(fs))
+                                await writer.WriteAsync(JsonConvert.SerializeObject(settings));
+                            break;
+                        }
+                        catch (IOException)
+                        {
+
+                            await Task.Delay(1000);
+                        }
+                    }
+                }
+
+                
+            }
+            public class SettingsService : IService
+            {
+                public void Install(DiscordClient client) { }
+                public SettingsManager<SettingsT> AddModule<ModuleT, SettingsT>(ModuleManager manager)
+                    where SettingsT : class, new()
+                    => new SettingsManager<SettingsT>(manager.Id);
+
+            }
+
+            public SettingsManager<GlobalSettingsT, SettingsT> AddModule<ModuleT, GlobalSettingsT, SettingsT>(ModuleManager manager)
+                where GlobalSettingsT : class, new()
+                where SettingsT : class, new()
+                => new SettingsManager<GlobalSettingsT, SettingsT>(manager.Id);
+
+        }
+
+    }
+}
